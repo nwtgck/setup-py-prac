d59bb478734178bfbf6f79502ec9371bf881f848
Ryo Ota <nwtgck@gmail.com>
2017-11-11 19:53:05 +0900

    [add] Add files related to test

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 # (from: https://github.com/masaponto/Python-MLP/blob/master/setup.py)
-from distutils.core import setup
+# (from: https://qiita.com/masashi127/items/5bfcba5cad8e82958844)
+
+from setuptools import setup, find_packages
+
+import sys
+
+sys.path.append('./src')
+sys.path.append('./tests')
 
 setup(
     name='nwtgck_hello',
@@ -9,5 +16,6 @@ setup(
     author_email='nwtgck@gmail.com',
     install_requires=['scikit-learn', 'numpy'],
     py_modules=["nwtgck_hello"],
-    package_dir={'': 'src'}
+    packages=find_packages(),
+    test_suite='nwtgck_hello_test.suite'
 )
